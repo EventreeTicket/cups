07d95adbb6e3bb5a99b81aa172690c01d16627cb
vincentelving <vincentelving@gmail.com>
2026-08-18 14:01:47 +0200

    Use public cups API in Android app

--- android-app/app/src/main/java/nl/hardcups/rfid/ApiConfig.java
+++ android-app/app/src/main/java/nl/hardcups/rfid/ApiConfig.java
@@ -1,12 +1,10 @@
 package nl.hardcups.rfid;
 
 /**
- * Wifi-adres van de computer waarop de lokale PHP-API draait. De Sunmi en deze
- * computer moeten op hetzelfde netwerk zitten. Pas dit adres aan wanneer het
- * netwerk een ander adres aan de computer geeft.
+ * Publieke HTTPS-API voor de hardcups-registratie.
  */
 final class ApiConfig {
-    static final String BASE_URL = "http://192.168.1.21:8080";
+    static final String BASE_URL = "https://cups.paytree-network.nl";
     static final String SOURCE = "sunmi-l3";
 
     private ApiConfig() { }
